df51792cadf28ac45ee9e4a69a0f770584223e68
namin-amin <navaneesh185@gmail.com>
2020-09-24 19:22:49 +0530

    custom buttons removed

--- public/electron.js
+++ public/electron.js
@@ -12,7 +12,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680, webPreferences: {
     nodeIntegration: true,
     enableRemoteModule: true
-},frame:false});
+}});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 }
--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,12 @@ import Calender from "./components/Workers/Calender/Calender";
 import Tasks from "./components/Workers/Tasks/Tasks";
 import Archived from "./components/Workers/Archived/Archived";
 import Tabs from './components/UI-components/Tab/Tabs'
-import Topbar from "./components/UI-components/Topbar/Topbar";
-
-
-// const electron = window.require("electron");
-// const ipcRenderer = electron.ipcRenderer;
-// const os = window.require('os');
 
 function App() {
-  // const pressed = () => {
-  //   ipcRenderer.send("off");
-  // };
-  // const minimize = () => {
-  //   ipcRenderer.send('minimize')
-
-  // // dialog.showOpenDialog({
-  // //     properties: ["openDirectory"],
-  // //   },).then(res=>alert(res.filePaths))
-  // //   .catch(err=>alert("something went wrong"));
-
-  // //   ;
-  // };
 
   return (
  
     <div className={classes.App}>
-    <Topbar />
     <Switch>
       <Route path="/" exact>
        <Tasks />
