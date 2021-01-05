3e95051cb09c8a668e40202b4c06346de6cea1f1
namin-amin <navaneesh185@gmail.com>
2021-01-05 13:42:59 +0530

    splash screen added

--- public/electron.js
+++ public/electron.js
@@ -9,16 +9,26 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680, webPreferences: {
+  mainWindow = new BrowserWindow({width: 900, height: 680, show:false, webPreferences: {
     nodeIntegration: true,
     enableRemoteModule: true
 }});
+
+  splashWin = new BrowserWindow({width:600,height:250,hasShadow:true,alwaysOnTop:true,show:false,frame:false})
+  splashWin.loadFile(__dirname+'/splash.html')
+  splashWin.on('ready-to-show',()=>splashWin.show())
+
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.removeMenu();
+
+  setTimeout(()=>{
+    splashWin.destroy()
+    mainWindow.show()
+  },6000)
   mainWindow.on('closed', () => mainWindow = null);
-}
-
-
+  }
+          
+  
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
